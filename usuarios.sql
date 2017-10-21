8a6aec361d2aca9fc5cb6b530b37d0661de0136a
RicardoLuis0 <ricolvs123@gmail.com>
2017-10-21 11:41:15 -0200

    Update usuarios.sql

--- usuarios.sql
+++ usuarios.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS 'CasaCuore'.'usuarios'(
-	'login' VARCHAR(64) NOT NULL,
-	'hash' VARCHAR(255) NOT NULL,
-	'tipo_conta' ENUM ('Pessoa','Educador'),
-	'cpf' VARCHAR (11) NOT NULL,
-	PRIMARY KEY('login'),
-	FOREIGN KEY('cpf') REFERENCES 'CasaCuore'.'pessoas'('cpf')
+CREATE TABLE IF NOT EXISTS CasaCuore.usuarios(
+	login VARCHAR(64) NOT NULL,
+	hash VARCHAR(255) NOT NULL,
+	tipo_conta ENUM ('Pessoa','Educador'),
+	cpf VARCHAR (11) NOT NULL,
+	PRIMARY KEY(login),
+	FOREIGN KEY(cpf) REFERENCES CasaCuore.pessoas(cpf)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
