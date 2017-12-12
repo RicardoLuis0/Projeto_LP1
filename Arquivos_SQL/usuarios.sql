15c82acf59d3e5acac62861e3110fb4e5d807e73
v10k <32981988+v10k@users.noreply.github.com>
2017-12-12 12:46:53 -0200

    Delete usuarios.sql

--- Arquivos_SQL/usuarios.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS CasaCuore.usuarios(
-	login VARCHAR(64) NOT NULL,
-	hash VARCHAR(255) NOT NULL,
-	tipo_conta ENUM ('Pessoa','Educador','Admin'),
-	cpf VARCHAR (11) NOT NULL,
-	PRIMARY KEY(login),
-	FOREIGN KEY(cpf) REFERENCES CasaCuore.pessoas(cpf)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
