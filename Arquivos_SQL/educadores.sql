5c457c86d982c13253b2b6474646712068ac51cc
v10k <32981988+v10k@users.noreply.github.com>
2017-12-12 12:46:32 -0200

    Delete educadores.sql

--- Arquivos_SQL/educadores.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS CasaCuore.educadores (
-	cpf CHAR(11) NOT NULL,
-	conta_bancaria VARCHAR(40),
-    salario FLOAT,
-	PRIMARY KEY (cpf),
-	FOREIGN KEY (cpf) REFERENCES CasaCuore.pessoas(cpf)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
